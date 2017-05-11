f2462989278fcd69bb871a876d204d3a052e35a3
meis5 <meis5@qq.com>
2017-05-11 23:23:41 +0800

    'df'

--- src/web/components/Navbar/Navbar.jsx
+++ src/web/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
-import React, { PureComponent } from 'react'
-import { Link, Route } from 'react-router-dom'
-import { Navbar, Nav, NavDropdown, MenuItem, NavItem } from 'react-bootstrap'
+import React, {PureComponent} from 'react'
+import {Link, Route} from 'react-router-dom'
+import {Navbar, Nav, NavDropdown, MenuItem, NavItem} from 'react-bootstrap'
 
 export default class NavBar extends PureComponent {
   logout = e => {
@@ -14,25 +14,10 @@ export default class NavBar extends PureComponent {
 
   render() {
     return (
-      <Navbar collapseOnSelect>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <a href="/"><span className="glyphicon glyphicon-home" /></a>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <ul className="nav navbar-nav" data-reactid="14">
-            <li><Link to="/userInfo">个人信息</Link></li>
-            <li><Link to="/agentRecharges">申请充值</Link></li>
-            <li><Link to="/playerRecharges">玩家充值</Link></li>
-            <li><Link to="/analytics">信息总计</Link></li>
-          </ul>
-          <ul className="nav navbar-nav navbar-right">
-            <li><a href="/" onClick={this.logout}>退出</a></li>
-          </ul>
-        </Navbar.Collapse>
-      </Navbar>
+      <div className="navbar__">
+        <a href="/">控制台</a>
+        <a href="/" onClick={this.logout}>退出</a>
+      </div>
     )
   }
 }
--- src/web/styles/index.js
+++ src/web/styles/index.js
@@ -1,13 +1,35 @@
 export default `
+.navbar__ {
+  height: 51px;
+  background: rgba(0,0,0,.7);
+  border-bottom: 1px solid rgba(0,0,0,.8);
+  margin-bottom: 20px;
+  clear: both;
+}
+
+.navbar__ a {
+  display: block;
+  height: 50px;
+  padding: 0 20px;
+  color: #fff;
+  line-height: 50px;
+  float: left;
+}
+
+.navbar__ a:last-child {
+  float: right;
+}
+
 .home-coin-ctn {
   border: 1px solid #ddd;
-  padding: 20px;
+  padding: 10px;
+  border-radius: 4px;
   color: #888;
 }
 
 .home-coin-ctn div {
   text-align: center;
-  font-size: 48px;
+  font-size: 36px;
   color: #CCAC00;
 }
 
@@ -20,7 +42,7 @@ export default `
   display: block;
   float: none;
   border: 1px solid #ddd;
-  margin-top: 40px;
+  margin-top: 20px;
   width: 100%;
   height: 80px;
   line-height: 80px;
@@ -28,6 +50,7 @@ export default `
 }
 
 .home-dash-board li a {
+  font-size: 16px;
   display: block;
   padding: 0 15px;
   text-align: center;
